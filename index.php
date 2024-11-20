7669d2e893e33767d660cae9d266de7e26081517
Al44n <alanmonzon640@gmail.com>
2024-11-20 18:51:31 -0300

    lo q se te da la gana
    espero no haberla cagado

--- index.php
+++ index.php
@@ -95,13 +95,48 @@
     </section>
 
     <section id="Inicio" class="Inicio">
-        <div class="Background">
-            <div class="content-inicio">
-                <div class="text-inicio">
-                    <H2 id="hola">HOLA SI</H2>
-                </div>
-            </div>
-        </div>
+
+    <div class="container">
+    <div class="line"></div>
+    <h1>Nuestros Productos</h1>
+    <p>
+        Nuestros productos combinan la mejor calidad y sabor, utilizando ingredientes sin gluten 
+        seleccionados cuidadosamente. Ofrecemos precios accesibles para que todos puedan disfrutar 
+        de opciones deliciosas y saludables, elaboradas con un toque artesanal.
+    </p>
+
+    <div class="linechica"></div>
+
+
+    <h2>Sobre Nosotros</h2>
+    <p>
+        Somos una panadería comprometida con ofrecer opciones sin gluten que deleiten a todos. 
+        Trabajamos con pasión y dedicación para garantizar productos de alta calidad, cuidando 
+        cada detalle en su elaboración. ¡Nuestra misión es llenar tu mesa de momentos inolvidables!
+    </p>
+    </div>
+    <div class="line"></div>
+    
+    <div class="cont">
+    <div class="info">
+      <h2>¿Donde Estamos?</h2>
+      <p><span class="highlight">VISÍTANOS EN NUESTRA ÚNICA DIRECCIÓN</span></p>
+      <p>Venezuela 2033, B1882 Quilmes, Provincia de Buenos Aires</p>
+      <p><span class="highlight">De Lunes a Viernes:</span> 9AM-2PM | 4PM-7:30PM</p>
+      <p><span class="highlight">Sábados:</span> 9AM-2PM</p>
+      <p><span class="highlight">Domingos y Feriados:</span> Cerrado</p>
+      <p><span class="highlight">WhatsApp:</span> <a href="https://wa.me/01154740106" target="_blank">011 5474-0106</a></p>
+    </div>
+
+    <iframe 
+    src="https://www.google.com/maps?q=https://maps.app.goo.gl/iYWBg14XJxrdgYyN6?g_st=com.google.maps.preview.copy&output=embed" 
+    width="600" 
+    height="400" 
+    style="border:0;" 
+    allowfullscreen="" 
+    loading="lazy">
+  </iframe>
+
     </section>
     <section id="Compra" class="Compra">
 
