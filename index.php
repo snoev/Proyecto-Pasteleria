5d3ee943f415d7bf8dd8c7c1371a861c27a900d0
MATT367 <mateuelcapo@gmail.com>
2024-11-22 12:35:00 -0300

    hola si subo esto

--- index.php
+++ index.php
@@ -177,8 +177,6 @@
             require_once 'assets/footer.php';
         ?>
 
-        <script src="js/index.js"></script>
-        <script src="js/inputValidator.js"></script>
         <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
         <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
     </body>
